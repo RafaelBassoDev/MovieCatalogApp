47c1a980d30aa71bdce1ccefbda004e8e38f4720
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-02-06 18:31:05 -0300

    feat: created MovieListViewController.

---
+++ MobileChallenge01/Controller/MovieListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListController.swift
+//  MobileChallenge01
+//
+//  Created by Rafael Basso on 06/02/23.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+    weak var coordinator: MainCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
