e3d80c4d8da614cc3862c40e38a01206ef3c869a
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-02-07 10:25:31 -0300

    feat: created 'Movie' model.

---
+++ MobileChallenge01/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  MobileChallenge01
+//
+//  Created by Rafael Basso on 07/02/23.
+//
+
+import UIKit.UIImage
+
+struct Movie {
+    var poster: UIImage
+    var title: String
+    var releaseDate: String
+}
