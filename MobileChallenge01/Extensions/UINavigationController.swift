791626e0927df064c36ec07b0928024fdacc9312
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-02-08 17:17:29 -0300

    refactor: moved appearance customization code from 'SceneDelegate' to an extension of 'UINavigationController'.

--- MobileChallenge01/Extensions/UINavigationController.swift
+++ MobileChallenge01/Extensions/UINavigationController.swift
@@ -5,4 +5,41 @@
 //  Created by Rafael Basso on 08/02/23.
 //
 
-import Foundation
+import UIKit.UINavigationController
+import UIKit.UINavigationBar
+
+extension UINavigationController {
+    func setCustomAppearance() {
+        navigationBar.setCustomCompactAppearance()
+        navigationBar.setCustomScrollEdgeAppearance()
+    }
+}
+
+extension UINavigationBar {
+    func setCustomCompactAppearance() {
+        let defaultAppearance = UINavigationBarAppearance()
+        
+        defaultAppearance.configureWithTransparentBackground()
+        defaultAppearance.backgroundColor = .systemGray6
+        defaultAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.black,
+            .font: UIFont.systemFont(ofSize: 22, weight: .bold)
+        ]
+        
+        self.standardAppearance = defaultAppearance
+        self.compactAppearance = defaultAppearance
+    }
+    
+    func setCustomScrollEdgeAppearance() {
+        let scrollAppearance = UINavigationBarAppearance()
+        
+        scrollAppearance.configureWithOpaqueBackground()
+        scrollAppearance.backgroundColor = .white
+        scrollAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.black,
+            .font: UIFont.systemFont(ofSize: 22, weight: .bold)
+        ]
+        
+        self.scrollEdgeAppearance = scrollAppearance
+    }
+}
