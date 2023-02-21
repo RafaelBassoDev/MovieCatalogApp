351c79ad0e5b3f92ba9cc74e138a88483807c5a7
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-02-21 13:06:49 -0300

    feat(MovieDetailViewController): added title UILabel as controller subView.

--- MobileChallenge01/Screens/MovieDetail/MovieDetailViewController.swift
+++ MobileChallenge01/Screens/MovieDetail/MovieDetailViewController.swift
@@ -12,13 +12,34 @@ class MovieDetailViewController: UIViewController {
     
     let viewModel: MovieDetailViewModel!
     
+    let titleUILabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     init(viewModel: MovieDetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        
+        NSLayoutConstraint.activate([
+            titleUILabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleUILabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         self.viewModel = nil
         super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        titleUILabel.text = viewModel.getMovieTitle()
+        
+        view.addSubview(titleUILabel)
     }
 }
