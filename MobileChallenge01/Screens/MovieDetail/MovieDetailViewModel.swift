ee668f8bfeb01bc061b08be4803866d84ea8d411
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-02-21 12:00:46 -0300

    feat: created 'MovieDetailViewModel'.

---
+++ MobileChallenge01/Screens/MovieDetail/MovieDetailViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailViewModel.swift
+//  MobileChallenge01
+//
+//  Created by Rafael Basso on 21/02/23.
+//
+
+import Foundation
+
+struct MovieDetailViewModel {
+    
+    var model: Movie
+    
+    init(model: Movie) {
+        self.model = model
+    }
+}
