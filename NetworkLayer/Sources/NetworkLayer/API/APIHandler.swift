5bda76ddc515020ca83e1e9a2932eeba298020f2
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-02-25 16:43:14 -0300

    refactor: moved funcionalities from 'APIHandler' to other files, and changed file hierarchy.

---
+++ NetworkLayer/Sources/NetworkLayer/API/APIHandler.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public struct APIHandler {
+    
+    private let endpoint: String
+    private let dataTaskhandler: APIDataTaskManager
+    
+    public init(endpoint: String, session: URLSession = URLSession.shared) {
+        self.endpoint = endpoint
+        self.dataTaskhandler = APIDataTaskManager(session: session)
+    }
+    
+    public func callAPI(arguments: String = "", completion: @escaping (Result<(Data?, URLResponse?), Error>) -> Void ) {
+        
+        let URLString = self.endpoint + arguments
+        
+        let url = URL(string: URLString)
+        
+        if let apiRequest = try? APIRequest(url: url) {
+            dataTaskhandler.createRequest(request: apiRequest.getRequest()) { result in
+                completion(result)
+            }
+        }
+    }
+}
