32befcaaecbe29b2519cf415d00a3ed4e034f339
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-02 19:58:49 -0300

    test(APIRequestTests): created tests to validate 'APIRequest' initialization and URL validation.

--- NetworkLayer/Tests/NetworkLayerTests/APIRequestTests.swift
+++ NetworkLayer/Tests/NetworkLayerTests/APIRequestTests.swift
@@ -6,15 +6,57 @@
 //
 
 import XCTest
+@testable import NetworkLayer
 
 final class APIRequestTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testCreateRequestWithNilURL() {
+        let url = URL(string: "")
+        
+        do {
+            let _ = try APIRequest(url: url)
+            
+        } catch {
+            if let requestError = error as? APIError {
+                XCTAssertEqual(requestError, APIError.badURL)
+            }
+        }
     }
     
+    func testCreateRequestWithInvalidURLs() {
+        let invalidURLArray = [
+            URL(string: "abcd"),
+            URL(string: "testURL"),
+            URL(string: "!@ˆ#(@*!$%ˆ)"),
+            URL(string: "google.c"),
+            URL(string: ".com"),
+            URL(string: "https://"),
+            URL(string: "http://"),
+            URL(string: "")
+        ]
+        
+        for invalidURL in invalidURLArray {
+            do {
+                let request = try APIRequest(url: invalidURL)
+                XCTAssertNil(request)
+                
+            } catch {
+                XCTAssertEqual(error as! APIError, APIError.badURL)
+            }
+        }
+    }
+    
+    func testCreateRequestWithValidURL() throws {
+        
+        let urlString = "www.google.com"
+        
+        let url = URL(string: urlString)
+        
+        let apiRequest = try APIRequest(url: url)
+        
+        let htmlRequest = apiRequest.getRequest()
+        
+        XCTAssertEqual(htmlRequest.url?.absoluteString, urlString)
+
+    }
 }
