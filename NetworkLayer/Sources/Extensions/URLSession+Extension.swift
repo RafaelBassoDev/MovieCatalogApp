7b7ad48e4b9f6d44f992a594d4cc283fba4e8c25
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-04 13:46:29 -0300

    feat: created 'URLSession+Extension', and extended funcionalities from 'APISession'.

---
+++ NetworkLayer/Sources/Extensions/URLSession+Extension.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension URLSession: APISession {
+    public func sendRequest(_ request: URLRequest, completion: @escaping (Data?, URLResponse?, Error) -> Void) {
+        let task = self.dataTask(with: request) { data, response, error in
+            completion(data, response, error)
+        }
+        
+        task.resume()
+    }
+}
