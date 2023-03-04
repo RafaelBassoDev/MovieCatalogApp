09b4499b87840c629726ede8a2b820ec836e8308
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-04 14:01:48 -0300

    refactor(APISession): completion handler 'Error' is now and optional.

--- NetworkLayer/Sources/NetworkLayer/Extensions/URLSession+Extension.swift
+++ NetworkLayer/Sources/NetworkLayer/Extensions/URLSession+Extension.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension URLSession: APISession {
-    public func sendRequest(_ request: URLRequest, completion: @escaping (Data?, URLResponse?, Error) -> Void) {
+    public func sendRequest(_ request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
         let task = self.dataTask(with: request) { data, response, error in
             completion(data, response, error)
         }
