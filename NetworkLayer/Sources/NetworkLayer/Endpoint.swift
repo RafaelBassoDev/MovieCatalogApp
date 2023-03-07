f3f72155028b3e78980471ade2cf6fd419e4ef89
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-07 15:34:03 -0300

    feat: created protocol 'Endpoint'.

---
+++ NetworkLayer/Sources/NetworkLayer/Endpoint.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+    
+}
