f214d90fd5c6f1e31e76194d5435cbea8ef0e1a3
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-07 15:34:20 -0300

    feat: created enum 'HTTPMetod'.

---
+++ NetworkLayer/Sources/NetworkLayer/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rafael Basso on 07/03/23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
