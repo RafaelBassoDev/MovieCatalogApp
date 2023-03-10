441785d0abc38e1e4e4520ae7d57cb2c94b65f69
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-10 17:22:14 -0300

    test: created new test case named 'JSONProtocolTests'.

---
+++ NetworkLayer/Tests/NetworkLayerTests/JSONProtocolTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+@testable import NetworkLayer
+
+class JSONFileLoadingMock: JSONFileLoading {}
+class JSONDataParsingMock: JSONDataParsing {}
+
+final class JSONProtocolTests: XCTestCase {
+    
+    var bundle: Bundle {
+        Bundle(for: type(of: self))
+    }
+    
+    var mockLoader = JSONFileLoadingMock()
+    var mockParser = JSONDataParsingMock()
+    
+    func testLoadInvalidJSONFile() {
+        
+        let invalidJSONName = "some_invalid_name"
+        
+        let loadedData = mockLoader.loadDataFromFile(named: invalidJSONName, bundle: bundle)
+        
+        XCTAssertNil(loadedData)
+    }
+
+    func testLoadValidJSONFile() {
+        
+        let validJSONName = "json_response"
+        
+        let loadedData = mockLoader.loadDataFromFile(named: validJSONName, bundle: bundle)
+        
+        XCTAssertNotNil(loadedData)
+    }
+    
+}
