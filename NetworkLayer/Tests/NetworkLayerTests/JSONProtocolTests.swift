571e68ee7094fba9d0fc71c95e2db315b308c860
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-10 17:49:35 -0300

    test(JSONProtocolTests): created new test cases for JSON parsing methods.

--- NetworkLayer/Tests/NetworkLayerTests/JSONProtocolTests.swift
+++ NetworkLayer/Tests/NetworkLayerTests/JSONProtocolTests.swift
@@ -4,6 +4,12 @@ import XCTest
 class JSONFileLoadingMock: JSONFileLoading {}
 class JSONDataParsingMock: JSONDataParsing {}
 
+class JSONResponse: Codable {
+    let id: Int
+    let title: String
+    let completed: Bool
+}
+
 final class JSONProtocolTests: XCTestCase {
     
     var bundle = Bundle.module
@@ -27,6 +33,41 @@ final class JSONProtocolTests: XCTestCase {
         let loadedData = mockLoader.loadDataFromFile(named: validJSONName, bundle: bundle)
         
         XCTAssertNotNil(loadedData)
+    }
+    
+    func testParseValidJSONFile() {
+        
+        let jsonFileName = "json_response"
+        
+        guard let loadedData = mockLoader.loadDataFromFile(named: jsonFileName, bundle: bundle) else {
+            XCTFail("Failed to load JSON file.")
+            return
+        }
+        
+        let parsedData = mockParser.parse(data: loadedData, from: JSONResponse.self)
+        
+        XCTAssertNotNil(parsedData)
+    }
+    
+    func testParsedJSONDataMatchesInitialData() {
+        
+        let data = """
+            {
+                "id": 1,
+                "title": "SomeTitle",
+                "completed": true
+            }
+            """.data(using: .utf8)
+        
+        guard let jsonData = data, let parsedData = mockParser.parse(data: jsonData, from: JSONResponse.self) else {
+            XCTFail("Failed to parse JSON data.")
+            return
+        }
+        
+        XCTAssertEqual(parsedData.id, 1)
+        XCTAssertEqual(parsedData.title, "SomeTitle")
+        XCTAssertEqual(parsedData.completed, true)
+        
     }
     
 }
