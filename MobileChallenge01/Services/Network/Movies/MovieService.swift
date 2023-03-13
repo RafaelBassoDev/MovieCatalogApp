ca131523b8db7902dc0af8d3153677f8fd6543b0
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-13 14:27:02 -0300

    feat(MovieService): struct now conforms to 'MovieServiceable'.

--- MobileChallenge01/Services/Network/Movies/MovieService.swift
+++ MobileChallenge01/Services/Network/Movies/MovieService.swift
@@ -1,6 +1,17 @@
 import Foundation
 import NetworkLayer
 
-struct MovieService: HTTPClient {
-    
+struct MovieService: HTTPClient, MovieServiceable {
+    func getPopular() async -> Result<PopularResponseModel, RequestError> {
+        do {
+            let response = try await sendRequest(endpoint: MovieEndpoint.popular, responseModel: PopularResponseModel.self)
+            return .success(response)
+        } catch {
+            if let requestError = error as? RequestError {
+                return .failure(requestError)
+            }
+        }
+        
+        return .failure(.unknown)
+    }
 }
