af9020d7f68f54b3fcd3b2d4d1bcb80f9786bab1
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-13 17:02:06 -0300

    feat(MovieRepository): implemented 'getPopularMovies' and related methods.

--- MobileChallenge01/Services/Repository/Movie/MovieRepository.swift
+++ MobileChallenge01/Services/Repository/Movie/MovieRepository.swift
@@ -3,11 +3,62 @@ import Foundation
 struct MovieRepository: MovieRepositoreable {
     typealias DataType = Movie
     
-    func getPopularMovies() async -> Result<[Movie], Error> {
-        return .success([])
+    private let service: MovieServiceable
+    
+    init(service: MovieServiceable) {
+        self.service = service
     }
     
-    func getGenres() async -> Result<[String], Error> {
-        return .success([])
+    func getPopularMovies() async -> Result<[Movie], Error> {
+        
+        let response = await service.getPopular()
+        let genres = await getGenres()
+        
+        switch response {
+        case .success(let popularMovies):
+            let movies = translateDomainMovies(from: popularMovies.results, genres: genres)
+            return .success(movies)
+            
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+}
+
+extension MovieRepository {
+    
+    private func getGenres() async -> [Genre] {
+        let response = await service.getGenres()
+        
+        switch response {
+        case .success(let model):
+            return model.genres
+            
+        case .failure:
+            return []
+        }
+    }
+    
+    private func translateDomainMovies(from serviceMovies: [MovieResponseModel], genres: [Genre]) -> [Movie] {
+        
+        var domainMovies = [Movie]()
+        
+        for serviceMovie in serviceMovies {
+            
+            let associatedGenres = genres.filter { serviceMovie.genreIDS.contains($0.id) }
+            
+            let movie = Movie(
+                poster: nil,
+                title: serviceMovie.title,
+                overview: serviceMovie.overview,
+                releaseDate: serviceMovie.releaseDate,
+                popularity: serviceMovie.popularity,
+                genres: associatedGenres.map { $0.name }
+            )
+            
+            domainMovies.append(movie)
+        }
+        
+        return domainMovies
     }
 }
