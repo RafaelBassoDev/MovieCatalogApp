a557876287658306819811037f10d5fcedeb94c5
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-13 17:31:46 -0300

    refactor(NetworkLayer/HTTPClient): 'sendRequest' now adds endoint 'query' into url.

--- NetworkLayer/Sources/NetworkLayer/Core/HTTPClient.swift
+++ NetworkLayer/Sources/NetworkLayer/Core/HTTPClient.swift
@@ -12,6 +12,7 @@ extension HTTPClient {
         urlComponents.scheme = endpoint.scheme
         urlComponents.host = endpoint.host
         urlComponents.path = endpoint.path
+        urlComponents.query = endpoint.query
         
         guard let url = urlComponents.url else {
             throw RequestError.invalidURL
