228a8349d73fb4e99d83ad27dbbcdc0cbd666822
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-13 15:52:33 -0300

    refactor(MovieService): created method 'handleRequest' to remove duplicated code.

--- MobileChallenge01/Services/Network/Movies/MovieService.swift
+++ MobileChallenge01/Services/Network/Movies/MovieService.swift
@@ -3,9 +3,24 @@ import NetworkLayer
 
 struct MovieService: HTTPClient, MovieServiceable {
     func getPopular() async -> Result<PopularResponseModel, RequestError> {
+        let endpoint = MovieEndpoint.popular
+        let responseModel = PopularResponseModel.self
+        
+        return await handleRequest(endpoint: endpoint, responseModel: responseModel)
+    }
+    
+    func getGenres() async -> Result<GenreResponseModel, RequestError> {
+        let endpoint = MovieEndpoint.genres
+        let responseModel = GenreResponseModel.self
+        
+        return await handleRequest(endpoint: endpoint, responseModel: responseModel)
+    }
+    
+    private func handleRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, RequestError> {
         do {
-            let response = try await sendRequest(endpoint: MovieEndpoint.popular, responseModel: PopularResponseModel.self)
+            let response = try await sendRequest(endpoint: endpoint, responseModel: responseModel.self)
             return .success(response)
+            
         } catch {
             if let requestError = error as? RequestError {
                 return .failure(requestError)
