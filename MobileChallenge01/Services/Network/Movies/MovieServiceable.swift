6a4f0a11dc18fa55842191e1254107dbfb3254ae
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-13 14:26:06 -0300

    feat(MovieServiceable): added method into protocol.

--- MobileChallenge01/Services/Network/Movies/MovieServiceable.swift
+++ MobileChallenge01/Services/Network/Movies/MovieServiceable.swift
@@ -1,8 +1,6 @@
-//
-//  MovieServiceable.swift
-//  MobileChallenge01
-//
-//  Created by Rafael Basso on 13/03/23.
-//
-
 import Foundation
+import NetworkLayer
+
+protocol MovieServiceable {
+    func getPopular() async -> Result<PopularResponseModel, RequestError>
+}
