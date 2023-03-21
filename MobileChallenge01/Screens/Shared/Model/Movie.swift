0d60526a0ca699aa0fab53857dac2b07dfdc04c0
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-21 16:08:02 -0300

    refactor(Movie & MovieList): both are now classes.

--- MobileChallenge01/Screens/Shared/Model/Movie.swift
+++ MobileChallenge01/Screens/Shared/Model/Movie.swift
@@ -7,12 +7,24 @@
 
 import UIKit.UIImage
 
-struct Movie {
-    let poster: UIImage?
+class Movie {
+    let id: Int
+    var poster: UIImage?
     let posterPath: String?
     let title: String
     let overview: String
     let releaseDate: String
     let popularity: Double
     let genres: [String]
+    
+    init(id: Int, poster: UIImage? = nil, posterPath: String?, title: String, overview: String, releaseDate: String, popularity: Double, genres: [String]) {
+        self.id = id
+        self.poster = poster
+        self.posterPath = posterPath
+        self.title = title
+        self.overview = overview
+        self.releaseDate = releaseDate
+        self.popularity = popularity
+        self.genres = genres
+    }
 }
