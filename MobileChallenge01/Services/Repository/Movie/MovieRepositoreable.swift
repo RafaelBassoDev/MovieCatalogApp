97e52f71525724218209659c259bab4b870efc43
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-21 16:05:38 -0300

    refactor(MovieRepositoreable): changed 'getMoviePoster' parameter.

--- MobileChallenge01/Services/Repository/Movie/MovieRepositoreable.swift
+++ MobileChallenge01/Services/Repository/Movie/MovieRepositoreable.swift
@@ -2,5 +2,5 @@ import UIKit
 
 protocol MovieRepositoreable {
     func getPopularMovies() async -> Result<[Movie], Error>
-    func getMoviePoster(movie: Movie, size: PosterSize) async -> UIImage?
+    func getMoviePoster(for movie: Movie, size: PosterSize) async -> UIImage?
 }
