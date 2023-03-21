abedd0439c5402a488b548bd3d3dae96aec7ae5d
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-21 12:28:25 -0300

    feat(MovieService): implemented method 'getPoster'.

--- MobileChallenge01/Services/Network/Movies/MovieService.swift
+++ MobileChallenge01/Services/Network/Movies/MovieService.swift
@@ -1,6 +1,10 @@
 import UIKit
 import NetworkLayer
 
+enum MovieServiceError: Error {
+    case badResponseData
+}
+
 struct MovieService: HTTPClient, MovieServiceable {
     func getPopular() async -> Result<PopularResponseModel, Error> {
         let endpoint = MovieEndpoint.popular
@@ -14,6 +18,20 @@ struct MovieService: HTTPClient, MovieServiceable {
         return await makeRequest(endpoint: endpoint, responseModel: responseModel)
     }
     
+    func getPoster(filePath: String, size: PosterSize = .original) async -> Result<UIImage, Error> {
+        let endpoint = MovieEndpoint.poster(size: size, path: filePath)
+        do {
+            let responseData = try await sendRequest(endpoint: endpoint)
+            guard let posterImage = UIImage(data: responseData) else {
+                return .failure(MovieServiceError.badResponseData)
+            }
+            return .success(posterImage)
+            
+        } catch {
+            return .failure(error)
+        }
+    }
+}
 
 extension MovieService {
     private func makeRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, Error> {
