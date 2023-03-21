2112324c3652e68786581027ade37aa95b685c53
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-21 12:15:32 -0300

    feat(MovieServiceable): added 'getPoster' method.

--- MobileChallenge01/Services/Network/Movies/MovieServiceable.swift
+++ MobileChallenge01/Services/Network/Movies/MovieServiceable.swift
@@ -1,7 +1,8 @@
-import Foundation
+import UIKit
 import NetworkLayer
 
 protocol MovieServiceable {
-    func getPopular() async -> Result<PopularResponseModel, RequestError>
-    func getGenres() async -> Result<GenreResponseModel, RequestError>
+    func getPopular() async -> Result<PopularResponseModel, Error>
+    func getGenres() async -> Result<GenreResponseModel, Error>
+    func getPoster(filePath: String, size: PosterSize) async -> Result<UIImage, Error>
 }
