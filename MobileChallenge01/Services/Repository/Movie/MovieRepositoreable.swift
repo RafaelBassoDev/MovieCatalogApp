c68084d9a550ff605b19dde53af9cb5195336e19
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-03-21 14:57:44 -0300

    feat(MovieRepositoreable): added 'getMoviePoster' to protocol.

--- MobileChallenge01/Services/Repository/Movie/MovieRepositoreable.swift
+++ MobileChallenge01/Services/Repository/Movie/MovieRepositoreable.swift
@@ -1,5 +1,6 @@
-import Foundation
+import UIKit
 
 protocol MovieRepositoreable {
     func getPopularMovies() async -> Result<[Movie], Error>
+    func getMoviePoster(movie: Movie, size: PosterSize) async -> UIImage?
 }
